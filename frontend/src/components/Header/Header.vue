b8a5b3154086af0ac59a93c66cc96595bd5c1722
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-04-26 17:51:05 -0300

    Update configurações

---
+++ frontend/src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    name: "Header"
+})
+export default class Header extends Vue {
+
+}
+</script>
+<style lang="sass">
+    
+</style>
