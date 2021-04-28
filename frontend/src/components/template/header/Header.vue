cf0ae073bec7d9e3284c15a4871de9370b4488f7
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-04-28 15:01:43 -0300

    Update header

--- frontend/src/components/template/header/Header.vue
+++ frontend/src/components/template/header/Header.vue
@@ -1,10 +1,12 @@
 <template>
     <header class="header">
-         <h1>Gerador de Tabelas</h1>
+         <router-link to="/"><h1 class="title">GERADOR DE TABELAS DE JOGOS</h1></router-link>
+         <a href="https://github.com/CassioFig/gerador-tabelas"><img class="img" src="../../../assets/github.svg"></a>
     </header>
 </template>
 
 <style lang="scss">
+
     .header {
         grid-area: header;
 
@@ -15,5 +17,21 @@
         width: 100%;
         height: 100%;
 
+    }
+
+    .title {
+        color: #ffffff;
+        font-family: 'Reem Kufi', sans-serif;
+    }
+
+    .img {
+        width: 60px;
+        margin-left: 20px;
+        margin-bottom: 10px;
+
+    }
+
+    a {
+        text-decoration: none;
     }
 </style>
