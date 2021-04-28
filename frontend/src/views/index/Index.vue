4a75e4ec85f10956089590753fbb3450bcd92a1b
Cássio Figueiredo <57011185+CassioFig@users.noreply.github.com>
2021-04-28 12:36:22 -0300

    Inclusão das opções para misturar time e ida e volta

--- frontend/src/views/index/Index.vue
+++ frontend/src/views/index/Index.vue
@@ -12,14 +12,20 @@
                     v-model="team">
                 <button class="index__card__button" @click="addTeam()">Adicionar</button>
 
+                <div class="index__card__checkboxes">
+                    <input type="checkbox" id="mix" v-model="listTeams.mix"> Misturar times?
+                    <input type="checkbox" id="secondTurn"  
+                        v-model="listTeams.secondTurn"> Ida e volta?
+                </div>
+
                 <div v-if="listTeams.teams.length >= 2">
                     <br>
                     <button class="index__card__button2" @click="generateGames()">Gerar Tabela</button>
                 </div>
 
             </div>
-            <br>
             <div>
+                <br>
                 <ul style="margin:0; padding:0">
                     <li 
                         v-for="team in listTeams.teams" :key="team"
@@ -45,18 +51,21 @@ import store from '../../store/index'
 export default class Index extends Vue {
 
     listTeams: any = {
-        teams: []
+        teams: [],
+        secondTurn: false,
+        mix: false
     };
+
     team: string = "";
 
-    addTeam() {
+    addTeam(): void {
         if(!this.listTeams.teams.includes(this.team)) {
             this.listTeams.teams.push(this.team);
             this.team = "";
         }
     }
 
-    generateGames() {
+    generateGames(): void {
         const url = "http://localhost:3000/api/generator";
         axios.post(url, this.listTeams)
             .then((response): void => {
